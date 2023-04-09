fc2bc82bad1ff37a5410eeaf6a99da16b92d6b23
ITP <RayhanUddinFarhad11@gmail.com>
2023-04-09 23:28:15 +0600

    Featured jobs section loading completed

--- src/components/Home/FeaturedJobs.jsx
+++ src/components/Home/FeaturedJobs.jsx
@@ -10,14 +10,21 @@ const FeaturedJobs = ({data}) => {
   <figure><img src="https://images.unsplash.com/photo-1496200186974-4293800e2c20?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80" alt="Shoes" /></figure>
   <div className="card-body text-left space-y-2">
     <h2 className="card-title">{data.job_title}</h2>
-    <p></p>
+    <p>{data.company_name}</p>
 
     <div className='flex space-x-5'>
 
 
-        <button className='outline outline-2 outline-indigo-400 px-2'>Full Time</button>
-        <button className='outline outline-2 outline-indigo-400 px-2'>Remote</button>
+        <button className='outline outline-2 outline-indigo-400 px-2'>{data.remote_or_onsite}</button>
+        <button className='outline outline-2 outline-indigo-400 px-2'>{data.fulltime_or_parttime}</button>
 
+    </div>
+
+    <div className='flex justify-around'>
+
+
+        <p>{data.location}</p>
+        <p>{data.salary}</p>
     </div>
 
     <div className=" justify-end">
