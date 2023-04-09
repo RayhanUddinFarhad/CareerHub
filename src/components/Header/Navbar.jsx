044d30688e2beae8c614860a5162b73a50a68c64
ITP <RayhanUddinFarhad11@gmail.com>
2023-04-10 03:30:33 +0600

    navbar responsive

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+    const [toggle, setToggle] = useState(false)
+
     return (
         <div className='flex justify-between items-center'>
 
@@ -11,7 +13,33 @@ const Navbar = () => {
                 <h1 className='text-4xl font-extrabold'>DevJobs</h1>
             </div>
 
-            <div className='space-x-6'>
+
+
+            <div className='lg:hidden mx-auto' onClick={() => setToggle(!toggle)}>
+
+{
+
+    toggle ? <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+  </svg> :
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+</svg>
+
+  
+
+
+
+}
+
+
+
+
+
+
+</div>
+
+            <div className={`flex lg:block flex-col lg:flex-wrap lg:flex-row static space-y-2 lg:space-y-0 lg:space-x-5 font-bold text-xl items-center  ${toggle ? 'block p-3 bg-white lg:bg-transparent w-full card-bordered lg:max-w-max ' : 'hidden'}`}>
 
 
                 <Link to = "/">Home</Link>
