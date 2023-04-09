2a25d568c89405c0509a41776bbab933cf410fec
ITP <RayhanUddinFarhad11@gmail.com>
2023-04-09 22:14:37 +0600

    Header section done

---
+++ src/components/Header/Banner.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div className='bg-blue-100 p-10'>
+
+           <div className='lg:flex items-center'>
+
+            <div className='text-left space-y-5'>
+
+                <h1 className='text-7xl font-bold '>
+
+                One Step <br /> Closer To Your <br /> <span className='text-indigo-400'>Dream Job</span>
+                </h1>
+                <p>Explore thousands of job opportunities with all the <br /> information you need. Its your future. Come find it. Manage all <br /> your job application from start to finish.</p>
+                <button className='btn-primary'>Get Started</button>
+
+
+
+            </div>
+
+
+            <div>
+
+
+                <img src="/src/assets/All Images/P3OLGJ1 copy 1.png" alt="" />
+            </div>
+
+
+            
+           </div>
+            
+        </div>
+    );
+};
+
+export default Banner;
---
+++ src/components/Header/Navbar.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <div className='flex justify-between items-center'>
+
+            <div>
+
+
+                <h1 className='text-4xl font-extrabold'>DevJobs</h1>
+            </div>
+
+            <div className='space-x-6'>
+
+
+                <Link>Home</Link>
+                <Link>Statics</Link>
+                <Link>Applied Jobs</Link>
+                <Link>Blog</Link>
+            </div>
+
+            <div>
+
+                <button className='btn-primary'>Start Applying</button>
+
+
+
+            </div>
+
+            
+        </div>
+    );
+};
+
+export default Navbar;
