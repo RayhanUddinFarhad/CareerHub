c957199539dd1f96e8576b178d6d1f10080e1680
ITP <RayhanUddinFarhad11@gmail.com>
2023-04-10 03:24:30 +0600

    made more responsive

--- src/components/Home/Category.jsx
+++ src/components/Home/Category.jsx
@@ -22,7 +22,7 @@ const Category = () => {
             <h1 className='text-5xl font-bold '>Job Category List</h1>
             <p>Explore thousands of job opportunities with all the information you need. Its your future</p>
 
-            <div className='grid grid-cols-4 gap-5'>
+            <div className='grid lg:grid-cols-4 gap-5'>
 
                 { category &&
 
